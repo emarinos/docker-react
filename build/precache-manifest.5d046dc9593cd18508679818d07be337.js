4aac86c75cf624946c87364646c218a42d9d13f8
emarinos <emarinos@icloud.com>
2020-03-08 18:56:26 +0100

    Initial commit

---
+++ build/precache-manifest.5d046dc9593cd18508679818d07be337.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7cb8fa8728e8bbef2cfdc4fffb7881",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d04073ff75f716af05d2",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d813e8bae67f74ea254c",
+    "url": "/static/js/2.71f3da36.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.71f3da36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d04073ff75f716af05d2",
+    "url": "/static/js/main.27b5fe16.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
